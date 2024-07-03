42bf0107306592358146ab0de9d42d0918c11376
uxert <150385101+uxert@users.noreply.github.com>
2024-07-03 15:16:24 +0200

    Added Kaiming and Xavier inits
    User can now choose in the console which init type he wants
    Using 'augmented' inits. i.e Kaiming for ReLU layer and Xavier for softmax layer allows to achieve same accuracy in fewer iterations

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def read_input_data():
             train_dataframe = pd.read_csv(f)
         with zip_ref.open(test_csv_name) as f:
             test_dataframe = pd.read_csv(f)
-    print(train_dataframe.head(10))
-    print(test_dataframe.head(10))
+    # print(train_dataframe.head(10))
+    # print(test_dataframe.head(10))
     train_array = np.array(train_dataframe, dtype=np.float32)
     test_array = np.array(test_dataframe, dtype=np.float32)
 
@@ -30,16 +30,33 @@ def read_input_data():
     return train_array, test_array
 
 
-def init_params(in_layer: int, hid_layer: int, out_layer: int):
+def init_params(in_layer: int, hid_layer: int, out_layer: int, augmented: bool = True):
     """
     initializes neural network parameters, i.e. its weights and biases using simple uniform dist with values [-0.5,0.5)
     :return: W1, B1, W2, B2 - numpy 2D arrays with sizes corresponding to given in_layer, hid_layer, out_layer
     """
-    # will try out Kaiming He initialization in the future
-    W1 = np.random.rand(hid_layer, in_layer) - 0.5
-    W2 = np.random.rand(out_layer, hid_layer) - 0.5
-    B1 = np.random.rand(hid_layer, 1) - 0.5
-    B2 = np.random.rand(out_layer, 1) - 0.5
+    # simple, uniform [-0.5; 0.5) dist
+    if augmented is False:
+        print("You chose uniform distribution init")
+        W1 = np.random.rand(hid_layer, in_layer) - 0.5
+        W2 = np.random.rand(out_layer, hid_layer) - 0.5
+        B1 = np.random.rand(hid_layer, 1) - 0.5
+        B2 = np.random.rand(out_layer, 1) - 0.5
+        return W1, B1, W2, B2
+
+    # kaiming He dist for the Relu layer
+    rng = np.random.default_rng()  # providing no seed so that each time it provides different weights and biases
+    mu = 0.0
+    sigmaW1 = np.sqrt(2/in_layer)
+    # sigmaW1 = 2/in_layer
+    W1 = rng.normal(loc=mu, scale=sigmaW1, size=(hid_layer, in_layer))
+    B1 = rng.normal(loc=mu, scale=sigmaW1, size=(hid_layer, 1))
+
+    # Xavier Golort init for the softmax layer
+    sigmaW2 = np.sqrt(2/(hid_layer+out_layer))
+    # sigmaW2 = 2/(hid_layer+out_layer)
+    W2 = rng.normal(loc=mu, scale=sigmaW2, size=(out_layer, hid_layer))
+    B2 = rng.normal(loc=mu, scale=sigmaW1, size=(out_layer, 1))
     return W1, B1, W2, B2
 
 
@@ -71,7 +88,9 @@ def softmax(x: npt.NDArray):
     :param x: numpy ndarray (it should be one-dimensional vector)
     :return: numpy ndarray of the same shape after performing softmax on it
     """
+    # substracting max allows to be certain that float will not overflow
     exponents = np.exp(x - np.max(x))
+    # exponents = np.exp(x)
     return exponents / np.sum(exponents, axis=None)
 
 
@@ -129,13 +148,14 @@ def one_hot_column(y, count):
     return arr
 
 
-def learn(lr, X, Y, W1, B1, W2, B2, iterations, batch_size, output_count):
+def train(lr, X, Y, W1, B1, W2, B2, iterations, batch_size, output_count):
     """
     this function is responsible for learning of the network using gradient descent.
     Returns network params afterwards
     :return: W1, B1, W2, B2
     """
     good_predictions = 0
+    predictions_count = 0
     for i in range(iterations):
         # pick random training examples to form a mini-batch. Usually training many times on smaller batches
         # works better than training fewer times using bigger batches
@@ -149,6 +169,7 @@ def learn(lr, X, Y, W1, B1, W2, B2, iterations, batch_size, output_count):
             y_vect = one_hot_column(y, output_count)
             L1, A1, L2, A2 = forward(x, W1, B1, W2, B2)
             prediction = np.argmax(A2)  # uses np.argmax to check which output is the most likely
+            predictions_count += 1
             if prediction == y:
                 good_predictions += 1
             # calculates all necessary gradients
@@ -158,7 +179,9 @@ def learn(lr, X, Y, W1, B1, W2, B2, iterations, batch_size, output_count):
 
         # prints current learning results from time to time
         if i > 0 and ((i < 500 and i % 50 == 0) or i % 200 == 0):
-            print(f"learning iteration:{i},\tlearning dataset accuracy: {good_predictions/(i * batch_size) * 100:.1f}%")
+            print(f"learning iteration:{i},\tlearning dataset accuracy: {good_predictions/predictions_count * 100:.1f}%")
+            good_predictions = 0
+            predictions_count = 0
 
     return W1, B1, W2, B2
 
@@ -187,15 +210,20 @@ def main():
 
     # declare hyperparameters
     INPUT_FEATURES_AMOUNT = X_train.shape[1]
-    hidden_layer_count: int = 10
+    hidden_layer_count: int = 12
     OUTPUT_LAYER_COUNT = 10  # for this use-case (handwritten digits) it will always be 10
     LEARNING_RATE = 0.1
 
+    print("Welcome to MNIST handwritten digits classifier!")
+    augmented_choice = "A"
+    choice = input(f"Which param init do you wish to use? print {augmented_choice} for augmented inits or print"
+                   f" anything else for simple uniform distribution\n")
+    iterations = int(input("How many iterations do you want to perform?\n"))
     # initialize network parameters
-    W1, B1, W2, B2 = init_params(INPUT_FEATURES_AMOUNT, hidden_layer_count, OUTPUT_LAYER_COUNT)
+    W1, B1, W2, B2 = init_params(INPUT_FEATURES_AMOUNT, hidden_layer_count, OUTPUT_LAYER_COUNT, augmented_choice == choice)
 
     # perform gradient descent
-    W1, B1, W2, B2 = learn(LEARNING_RATE, X_train, Y_train, W1, B1, W2, B2, 6000, 32, OUTPUT_LAYER_COUNT)
+    W1, B1, W2, B2 = train(LEARNING_RATE, X_train, Y_train, W1, B1, W2, B2, iterations, 64, OUTPUT_LAYER_COUNT)
 
     # test the network on another dataset
     test_network(X_test, Y_test, W1, B1, W2, B2)
